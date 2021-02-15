c51f04423db194acf786d56fc2bd5eccf633f69d
Jigar Chandra <chandra.67@osu.edu>
2021-02-15 12:03:07 +0530

    Update config.js

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -8,7 +8,8 @@ module.exports = {
     options: {
       dialect: "postgres",
       dialectOptions: {
-        multipleStatements: true
+        multipleStatements: true,
+        ssl: true
       }
     }
   }
