b9008576fd51f7f78bd59388d6c565ae442e7ace
Jigar Chandra <chandra.67@osu.edu>
2021-02-15 12:10:27 +0530

    Update config.js

--- api/server/src/config/config.js
+++ api/server/src/config/config.js
@@ -10,7 +10,7 @@ module.exports = {
       dialectOptions: {
         multipleStatements: true,
         ssl: true,
-        rejectUnauthorized: true
+        rejectUnauthorized: false
       }
     }
   }
